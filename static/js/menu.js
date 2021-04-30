3166178cbcc0c4b7aead16c3ac2b5a6e237f594a
JaimeFlack <ja.flack@duocuc.cl>
2021-04-30 12:15:43 -0400

    First commit

---
+++ static/js/menu.js
@@ -0,0 +1,21 @@
+function abrirMenu(){
+    var btn_menu = document.getElementById("btn-menu");
+    var menu = document.getElementById("menu");
+    var btn_close_menu = document.getElementById("btn-close-menu");
+
+    btn_menu.addEventListener("click", function(e){
+        menu.style.top = "100px";
+        menu.style.transition = ".4s ease all";        
+    });
+    btn_close_menu.addEventListener("click", function(e){
+        e.preventDefault();
+        menu.style.top = "-500px";
+        menu.style.transition = ".5s ease all";
+    });
+}
+abrirMenu();
+
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
